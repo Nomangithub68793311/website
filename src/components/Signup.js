3cf36f9fb03e5b922fa4fcce8ea6a746117fbaf0
unknown <rana ahamed>
2021-10-06 16:30:24 +0600

    added

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -67,6 +67,17 @@ const clickedsocial=(url,usernam,group,placeholder)=>{
   else if(placeholder=="Email Address"){
     return window.open(`${url}${usernam}`,'_self')
   }
+  else if(url=='https://snapchat.com/'||url=='https://www.tiktok.com/'){
+    const url1=url.split( 'https://' )[1]
+    const url2=url1.split( '.com/' )[0]
+    const url3=	 url.split( '/' )[3]
+    const url4=`${url2}://user?username=${url3}`
+    
+    return window.open(`${url4}`,'_self')
+  
+   
+    
+  }
   else if(placeholder=="Number Number"){
     return window.open(`${url}${usernam}`,'_self')
   }
